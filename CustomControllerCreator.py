b4df34eeaa9f591909f71b156693f7d07dbb54d5
shaneantrim <shaneantrim@gmail.com>
2023-04-19 21:41:31 -0700

    Changed name

---
+++ CustomControllerCreator.py
@@ -0,0 +1,2 @@
+# Hello and welcome, this is a tool for Maya that allows the user to create custom controllers for the rig, with options to adjust the size, shape, and color of the controllers.
+
--- RenameTool.py
+++
@@ -1,2 +0,0 @@
-# Hello and welcome, this is a tool for Maya that can be used to automate the process of renaming objects in Maya.
-
