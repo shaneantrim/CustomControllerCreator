74edbcdfcb66e126089a4ffed9a553c05f67a764
JessieDeJong <131053169+JessieDeJong@users.noreply.github.com>
2023-04-20 16:05:37 +0200

    Create RenameTool.py

---
+++ RenameTool.py
@@ -0,0 +1,10 @@
+#Auto Suffix/Prefix String
+renamer_settings = {'transform_suffix': '_tfm',
+                    'mesh_suffix': '_geo',
+                    'nurbs_crv_suffix': '_crv',
+                    'joint_suffix': '_jnt',
+                    'locator_suffix': '_loc',
+                    'surface_suffix': '_sur',
+                    'left_prefix': 'left_',
+                    'right_prefix': 'right_',
+                    'center_prefix': 'center_'}
