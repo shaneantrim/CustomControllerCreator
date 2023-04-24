843836d36ed620a5b1b0cd46eef74cb4f9156b22
shaneantrim <shaneantrim@gmail.com>
2023-04-23 17:24:37 -0700

    Update RenameTool.py
    Added a simple menu with four fields using PyQt5.

--- RenameTool.py
+++ RenameTool.py
@@ -1,10 +1,42 @@
-#Auto Suffix/Prefix String
-renamer_settings = {'transform_suffix': '_tfm',
-                    'mesh_suffix': '_geo',
-                    'nurbs_crv_suffix': '_crv',
-                    'joint_suffix': '_jnt',
-                    'locator_suffix': '_loc',
-                    'surface_suffix': '_sur',
-                    'left_prefix': 'left_',
-                    'right_prefix': 'right_',
-                    'center_prefix': 'center_'}
+import maya.cmds as cmds
+
+# Define the function that will be called when the user clicks the "Rename" button
+def rename_objects():
+    # Get the current selection
+    selection = cmds.ls(selection=True)
+    if not selection:
+        cmds.warning("Please select at least one object to rename.")
+        return
+    
+    # Get the user's input from the UI
+    prefix = cmds.textFieldGrp("prefixField", query=True, text=True)
+    suffix = cmds.textFieldGrp("suffixField", query=True, text=True)
+    replace = cmds.textFieldGrp("replaceField", query=True, text=True)
+    replace_with = cmds.textFieldGrp("replaceWithField", query=True, text=True)
+    
+    # Loop through each selected object and rename it
+    for obj in selection:
+        # Generate the new name based on the user's input
+        new_name = obj
+        if prefix:
+            new_name = prefix + new_name
+        if suffix:
+            new_name = new_name + suffix
+        if replace:
+            new_name = new_name.replace(replace, replace_with)
+        
+        # Rename the object
+        try:
+            cmds.rename(obj, new_name)
+        except:
+            cmds.warning("Could not rename object {0} to {1}.".format(obj, new_name))
+
+# Create the UI
+cmds.window(title="Rename Tool")
+cmds.columnLayout(adjustableColumn=True)
+cmds.textFieldGrp("prefixField", label="Prefix")
+cmds.textFieldGrp("suffixField", label="Suffix")
+cmds.textFieldGrp("replaceField", label="Replace")
+cmds.textFieldGrp("replaceWithField", label="Replace With")
+cmds.button(label="Rename", command=rename_objects)
+cmds.showWindow()
